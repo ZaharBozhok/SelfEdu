eeae7ac10f812730e7075fe4b7cbe6cc9034a0af
Bozhok Zahar <3aharbozhok@gmail.com>
2019-07-09 11:42:22 +0300

    Added rename functional

--- Patterns/DirectoryWalker/src/Filesystem/File.cpp
+++ Patterns/DirectoryWalker/src/Filesystem/File.cpp
@@ -1,4 +1,6 @@
 #include "File.h"
+#include <stdio.h>
+#include <string.h>
 
 namespace fs = FileSystem;
 
@@ -32,11 +34,16 @@ const std::string& fs::File::Name() const
 }
 void fs::File::Name(const std::string& newName)
 {
+    char* pathDub = strdup(m_path.c_str());
+    char* dir = dirname(pathDub);
+    std::string newPath = std::string(dir) + '/' + newName;
+    if(0 != rename(m_path.c_str(), newPath.c_str()))
+    {
+        throw std::runtime_error("Can't rename file");
+    }
     m_name = newName;
-}
-void fs::File::Commit()
-{
-    /* TODO: save file changes */
+    m_path = newPath;
+    delete[] pathDub;
 }
 void fs::File::getFileMode()
 {
